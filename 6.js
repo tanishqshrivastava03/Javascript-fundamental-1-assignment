549c9cb11946644f382b33561c1f4244b49f0e6b
Tanishq shrivastava <130395729+tanishqshrivastava03@users.noreply.github.com>
2024-05-18 18:18:41 +0530

    Create 6.js

---
+++ 6.js
@@ -0,0 +1,15 @@
+let Name = "Mithun";
+let email ="mithu.s@pw.live";
+let age = 21;
+if(typeof Name !== "string"){
+    console.log("Name should be string");
+}
+else if(typeof email !=="string"){
+    console.log("email should be string");
+}
+else if(typeof age !=="number"){
+    console.log("age should be number");
+}
+else{
+    console.log("fine");
+}
