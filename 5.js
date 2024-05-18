01554543573ee8d1ca5d61ce41da7371bae9b36a
Tanishq shrivastava <130395729+tanishqshrivastava03@users.noreply.github.com>
2024-05-18 18:18:21 +0530

    Create 5.js

---
+++ 5.js
@@ -0,0 +1,14 @@
+let package = "standard";
+switch (package) {
+  case "standard":
+    console.log("time taken :3-5 Days");
+    break;
+  case "express":
+    console.log("time taken :1-2 Days");
+    break;
+  case "overnight":
+    console.log("time taken: tomorrow");
+    break;
+  default:
+    console.log("Invalid package type!!");
+}
