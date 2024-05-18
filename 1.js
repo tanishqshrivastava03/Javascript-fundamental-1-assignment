3183dab69f3383cbb7ec3724ad5e8202a8ce0157
Tanishq shrivastava <130395729+tanishqshrivastava03@users.noreply.github.com>
2024-05-18 18:14:16 +0530

    Create 1.js
    first

---
+++ 1.js
@@ -0,0 +1,5 @@
+let totalValue = 2000;
+let discountPercentage = 20;
+
+let totalPrize = totalValue -(discountPercentage/100)*totalValue;
+console.log("The final prixe after discount is : Rs."+totalPrize);
